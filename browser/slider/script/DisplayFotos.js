dd661d585c70cfe09ac367a0021a460e2c630b81
kav.develop <kav.develop@yandex.ru>
2019-04-22 14:50:27 +0700

    Сделана задача 1 "Слайдер"

---
+++ browser/slider/script/DisplayFotos.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const FOTOFILES = ["airmax-jump.png",
+"airmax-on-foot.png",
+"airmax-playground.png",
+"airmax-top-view.png",
+"airmax.png"];
+
+var objFoto = document.getElementById('slider');
+var i = 0;  // счетчик фотографий в массиве FOTOFILES
+
+setInterval((list) => {
+    if (i < list.length) {
+        objFoto.src = 'i/' + list[i];
+        i++;
+    } else {
+        i = 0;
+    }
+}, 5000, FOTOFILES);
