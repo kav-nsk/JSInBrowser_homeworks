c930e8c93f7b2678c483e36be580896cf97055c8
kav.develop <kav.develop@yandex.ru>
2019-04-29 13:31:05 +0700

    улучшено

--- html-element-collection/drum-machine/script.js
+++ html-element-collection/drum-machine/script.js
@@ -3,8 +3,6 @@ const MUSICBUTTONS = document.getElementsByClassName('drum-kit__drum');
 
 for (let i of MUSICBUTTONS) {
     i.onclick = () => {
-        let newPlay = i.getElementsByTagName('audio')[0];
-        newPlay.type = 'audio/wav';
-        newPlay.play();
+        i.getElementsByTagName('audio')[0].play();
     }
 }
