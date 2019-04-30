13b090aabef7131f59d1a5db265ece84becff2bb
kav-nsk <kav.develop@yandex.ru>
2019-05-01 01:49:38 +0700

    Сделано

--- html-element-collection/audioplayer/script.js
+++ html-element-collection/audioplayer/script.js
@@ -1,15 +1,23 @@
 'use strict'
+
 const TRACKS = [
     'LA Chill Tour.mp3',
     'LA Fusion Jam.mp3',
     'This is it band.mp3'
 ];
-let currentTrack = TRACKS[0];
-const player = document.getElementsByTagName('audio')[0];
 
+let currentTrackPos = 0;    // Номер позиции текущего трека в TRACKS
+let curTime = 0;            // Временная метка для трека
+
+let playerElem = document.getElementsByTagName('audio')[0]; // Элемент плеера
+// Элемент динамичой картинки проигрывателя
+let playerPicture = document.getElementsByClassName('mediaplayer')[0];
+// Элемент отображения названия трека
+let trackName = document.getElementsByTagName('span')[0];
 // Коллекция кнопок управления плеером
 const BUTTONS = document.getElementsByTagName('button');
-
+// Элемент нопки play/pause
+let elem = BUTTONS[1].getElementsByTagName('i');
 
 // Кнопки + обработчики событий
 BUTTONS[0].onclick = back;
@@ -20,23 +28,48 @@ BUTTONS[3].onclick = next;
 
 // Функции - обработчики событий кнопок управления плеером
 function playstate() {
-    console.log('playstate');
+    if (elem[0].classList.toggle('fa-pause')) {
+        player(currentTrackPos, curTime);
+    } else {
+        curTime = playerElem.currentTime;
+        playerElem.pause();
+        playerPicture.classList.remove('play');
+    };
 }
 
 function stop() {
-    console.log('stop');
+    playerElem.pause();
+    curTime = 0;
+    elem[0].className = 'fa fa-play';
+    playerPicture.classList.remove('play');
 }
 
 function next() {
-    console.log('next');
+    currentTrackPos ++;
+    if (currentTrackPos === TRACKS.length) {
+        currentTrackPos = 0;
+    }
+    stop();
+    playstate();
+    let text = TRACKS[currentTrackPos];
+    trackName.title = text.slice(0, text.length - 4);
 }
 
 function back() {
-    console.log('back');
+    currentTrackPos --;
+    if (currentTrackPos < 0) {
+        currentTrackPos = TRACKS.length - 1;
+    }
+    stop();
+    playstate();
+    let text = TRACKS[currentTrackPos];
+    trackName.title = text.slice(0, text.length - 4);
 }
 
-
-// Вспомогательные функции
-function trackName() {
-
+// Остальные функции
+function player(pos, time = 0) {
+    playerElem.src = 'mp3/' + TRACKS[pos];
+    playerElem.currentTime = time;
+    playerElem.play();
+    playerPicture.classList.add('play');
 }
