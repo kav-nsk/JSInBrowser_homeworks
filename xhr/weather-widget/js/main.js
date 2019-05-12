2c8c49ebfbcb559df215f4e8c7aabdd2050c87ae
kav-nsk <kav.develop@yandex.ru>
2019-05-12 16:30:41 +0700

    Не уверен, что правильно понял задние, но похоже работает как требуется.

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,14 @@
+// Для проверки отклика на события.
+document.addEventListener('click', () => {
+  console.log('CLICK');
+});
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+request.addEventListener('load', () => {
+  if (request.status === 200) {
+    const response = JSON.parse(request.responseText);
+    setData(response);
+  }
+});
+request.open('GET', 'https://neto-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
-}
