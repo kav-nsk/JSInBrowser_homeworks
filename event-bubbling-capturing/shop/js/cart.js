59c9aa07d0ef2ba9ae65a43d69f4c7bfc8dc805a
kav-nsk <kav.develop@yandex.ru>
2019-05-19 22:42:24 +0700

    Сделана задача 2.

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Присвоение обработчиков событий.
+showMore.addEventListener('click', setEventToCartButtons);
+
+// Основной поток.
+setEventToCartButtons();
+ 
+// Функции-обработчики событий.
+function setEventToCartButtons() {
+    let addToCartButtons = list.getElementsByClassName('add-to-cart');
+    for (let button of addToCartButtons) {
+        button.addEventListener('click', event => {
+            event.preventDefault();
+            let item = {};
+            item.title = event.currentTarget.dataset.title;
+            item.price = event.currentTarget.dataset.price;
+            addToCart(item);  
+        });
+    }
+}
