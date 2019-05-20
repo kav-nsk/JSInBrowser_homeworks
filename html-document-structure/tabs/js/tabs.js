56c613b8e026a06d2c753b3c5e6b808b2d6402cb
kav.develop <kav.develop@yandex.ru>
2019-05-20 13:35:39 +0700

    Улучшен подход к работе с классами.

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // Интерфейс html страницы.
 let tabBar = document.getElementsByClassName('tabs-nav')[0];
 let tabsContent = document.getElementsByClassName('tabs-content')[0].childNodes;
@@ -6,13 +6,13 @@ let tabTemplate = document.querySelector('.tabs-nav li').cloneNode(true);
 
 // Глобальные объявления.
 
-// Привязка обработчиков событий
+// Привязка обработчиков событий.
 tabBar.addEventListener('click', setActiveTab);
 
 // Основной поток.
     // Удаляем заготовку из разметки.
 tabBar.removeChild(tabBar.firstElementChild);
-    // Работаем только с <article> без #text.
+    // Работаем только с < > без #text.
 for (let i of tabsContent) {
     if (i.childNodes.length != 0) {
         tabTemplate.firstElementChild.innerText = i.dataset.tabTitle;
@@ -20,17 +20,17 @@ for (let i of tabsContent) {
         tabBar.appendChild(tabTemplate.cloneNode(true));
     }
 }
-tabBar.firstElementChild.classList = 'ui-tabs-active'; // первая вкладка активная
+tabBar.firstElementChild.classList.add('ui-tabs-active');// первая вкладка активная
 setText(); // Делаем видимой только первую статью.
 
 
 // Функции-обработчики событий.
 function setActiveTab(event) {
-    // Обрабатываем только на <a>.
+    // Отрабатываем только на <a>.
     if (event.target.getElementsByTagName('a').length == 0) {
         for (let i of tabBar.childNodes) {
-            if (i.childNodes.length != 0 && i.classList == 'ui-tabs-active') {
-                i.classList = '';
+            if (i.childNodes.length != 0 && i.classList.contains('ui-tabs-active')) {
+                i.classList.remove('ui-tabs-active');
             }
         }
         event.target.parentElement.classList.add('ui-tabs-active');
@@ -40,13 +40,14 @@ function setActiveTab(event) {
 
 
 // Прочие функции.
+    // Показываем текст активной вкладки и скрываем остальной.
 function setText () {
     for (let i of tabBar.childNodes) {
-        if (i.childNodes.length != 0 && i.classList == 'ui-tabs-active') {
+        if (i.childNodes.length != 0 && i.classList.contains('ui-tabs-active')) {
             for (let j of tabsContent) {
                 if (j.childNodes.length != 0 &&
                     j.dataset.tabTitle.toUpperCase() != i.firstElementChild.innerText) {
-                    j.classList = 'hidden';
+                    j.classList.add('hidden');
                 } else {
                     j.classList = '';
                 }
