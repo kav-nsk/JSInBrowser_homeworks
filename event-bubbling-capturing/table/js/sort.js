666fdcdbbaeb2fd2cd65fb7c764514804d43bda7
kav.develop <kav.develop@yandex.ru>
2019-05-20 13:08:12 +0700

    Сделано!

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if (event.target.className == 'prop__name') {
+        if (!event.target.hasAttribute('data-dir')) {
+            event.target.setAttribute('data-dir', 1);
+        } else {
+            event.target.dataset.dir *= -1;
+        }
+        if (!table.hasAttribute('data-sort-by')) {
+            table.setAttribute('data-sort-by', event.target.dataset.propName);
+        } else {
+            table.dataset.sortBy = event.target.dataset.propName;
+        }
+        sortTable(event.target.dataset.propName, event.target.dataset.dir);
+    }
+
+
 }
