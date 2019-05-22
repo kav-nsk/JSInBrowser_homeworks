40dc0d768ed52e09e22e284770f672d86bb13f8d
kav.develop <kav.develop@yandex.ru>
2019-05-22 18:13:22 +0700

    Начато на работе.

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,1 +1,4 @@
 'use strict';
+// Заготовки html разметки.
+let colorHtml = '<div data-value="red" class="swatch-element color red available"><div class="tooltip">Красный</div><input quickbeam="color" id="swatch-1-red" type="radio" name="color" value="red" checked><label for="swatch-1-red" style="border-color: red;"><span style="background-color: red;"></span><img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886"></label></div>';
+
