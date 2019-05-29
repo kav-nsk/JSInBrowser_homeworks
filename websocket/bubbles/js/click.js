d1e6e82e40a9f681771316e8531db9fb2fca9c4f
kav-nsk <kav.develop@yandex.ru>
2019-05-29 21:45:02 +0700

    Сделаны шарики, прикольно!

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let connectWebSocket = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+document.addEventListener('click', sendCoord);
+
+showBubbles(connectWebSocket);
+
+function sendCoord(event) {
+    connectWebSocket.send(JSON.stringify({'x': event.clientX, 'y': event.clientY}));
+}
