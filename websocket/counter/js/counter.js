3468d93be9aeec60c0897e1eda82fca1af4c159e
kav-nsk <kav.develop@yandex.ru>
2019-05-30 08:15:25 +0700

    Сделано.

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+// Интерфейс.
+let numNet = document.querySelector('.counter');
+let numErr = document.querySelector('output.errors');
+
+let connWebSock = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connWebSock.addEventListener('message', event => {
+    let data = JSON.parse(event.data);
+    numNet.innerText = data.connections;
+    numErr.innerText = data.errors;
+
+});
+window.addEventListener('beforeunload', () => {
+    connWebSock.close(1000);
+});
